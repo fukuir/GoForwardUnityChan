9da3a00d8a9d663271761ef881a8aaf72fac5fdb
FukuiRyoutarou <calicocatff37@icloud.com>
2018-09-28 14:31:40 +0900

    GoForwardUnityChan
    課題用

---
+++ Assets/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
